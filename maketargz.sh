9613177c7a64be8fb0fa70eefe27da0b0b9805c3
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2007-02-25 22:33:54 +0000

    Package generator.

---
+++ maketargz.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+PACKAGE=lua-espeak
+VERSION=1.20r1-alpha
+
+DIRNAME=$PACKAGE-$VERSION
+TGZNAME=$DIRNAME.tar.gz
+
+lua doc.lua > doc/index.html
+
+rm -f $TGZNAME
+mkdir $DIRNAME
+
+cp -r COPYING $DIRNAME
+cp -r Makefile $DIRNAME
+cp -r README $DIRNAME
+cp -r luaespeak.c $DIRNAME
+cp -r test_espeak.lua $DIRNAME
+
+cp -r demos $DIRNAME
+rm -rf $DIRNAME/demos/.svn
+
+cp -r doc $DIRNAME
+rm -rf $DIRNAME/doc/.svn
+
+tar -czf $TGZNAME $DIRNAME
+rm -rf $DIRNAME
+tar -tzf $TGZNAME
