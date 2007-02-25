827922bed32c2464a1ff7e2ac3b9f70ac1a19587
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2007-02-25 14:57:12 +0000

    Moving some saytime code to its own module and adding a formal mode.

--- demos/clock_pt.lua
+++ demos/clock_pt.lua
@@ -6,67 +6,7 @@
 -- Distributed under the GPL v2 or later.
 
 require "espeak"
-
-local function saytime(h, m)
-    local hs = { "uma", "duas", "três", "quatro", "cinco", "seis", "sete",
-        "oito", "nove", "dez", "onze" }
-
-    local mt
-    if m > 30 then
-        h = h + 1
-        if h == 24 then
-            return (60 - m) .. " para a meia-noite"
-        elseif h == 1 then
-            return (60 - m) .. " para a uma da manhã"
-        elseif h > 1 and h < 12 then
-            return (60 - m) ..  " para as " .. hs[h] .. " da manhã"
-        elseif h == 12 then
-            return (60 - m) .. " para o meio-dia"
-        elseif h == 13 then
-            return (60 - m) .. " para a uma da tarde"
-        elseif h > 13 and h < 19 then
-            return (60 - m) ..  " para as " .. hs[h - 12] .. " da tarde"
-        else
-            return (60 - m) ..  " para as " .. hs[h - 12] .. " da noite"
-        end
-    else
-        if m == 30 then
-            if h == 0 then
-                return "meia-noite e meia"
-            elseif h == 12 then
-                return "meio-dia e meio"
-            elseif h > 12 then
-                mt = hs[h - 12] .. " e meia"
-            else
-                mt = hs[h] .. " e meia"
-            end
-        else
-            if h == 0 then
-                mt = "meia-noite"
-            elseif h == 12 then
-                mt = "meio-dia"
-            elseif h > 12 then
-                mt = hs[h - 12]
-            else
-                mt = hs[h]
-            end
-            if m ~= 0 then
-                mt = mt .. " e " .. m
-            end
-            if h == 0 or h == 12 then
-                return mt
-            end
-        end
-        if h < 12 then
-            return mt .. " da manhã"
-        elseif h > 13 and h < 19 then
-            return mt .. " da tarde"
-        else
-            return mt .. " da noite"
-        end
-    end
-end
-
+require "saytime"
 
 espeak.Initialize(espeak.AUDIO_OUTPUT_PLAYBACK, 500)
 
@@ -76,7 +16,7 @@ if espeak.SetVoiceByName("brazil") ~= espeak.EE_OK then
 end
 
 local dt = os.date("*t")
-espeak.Synth(saytime(dt.hour, dt.min))
+espeak.Synth(saytime.pt_spoken(dt.hour, dt.min))
 
 espeak.Synchronize()
 espeak.Terminate()
---
+++ demos/saytime.lua
@@ -0,0 +1,93 @@
+-- -*- coding: utf-8 -*-
+--
+-- Functions to "say" the time of the day.
+-- (c) 2007 Alexandre Erwin Ittner <aittner@netuno.com.br>
+--
+-- This file is part of Lua-eSpeak and is distributed under the GNU GPL v2
+-- or, at your option, any later version.
+--
+--
+
+module("saytime", package.seeall)
+
+-- Spoken Portuguese
+
+local pt_hs = { "uma", "duas", "três", "quatro", "cinco", "seis", "sete",
+    "oito", "nove", "dez", "onze" }
+
+function pt_spoken(h, m)
+    local mt
+    if m > 30 then
+        h = h + 1
+        if h == 24 then
+            return (60 - m) .. " para a meia-noite"
+        elseif h == 1 then
+            return (60 - m) .. " para a uma da manhã"
+        elseif h > 1 and h < 12 then
+            return (60 - m) ..  " para as " .. pt_hs[h] .. " da manhã"
+        elseif h == 12 then
+            return (60 - m) .. " para o meio-dia"
+        elseif h == 13 then
+            return (60 - m) .. " para a uma da tarde"
+        elseif h > 13 and h < 19 then
+            return (60 - m) ..  " para as " .. pt_hs[h - 12] .. " da tarde"
+        else
+            return (60 - m) ..  " para as " .. pt_hs[h - 12] .. " da noite"
+        end
+    else
+        if m == 30 then
+            if h == 0 then
+                return "meia-noite e meia"
+            elseif h == 12 then
+                return "meio-dia e meio"
+            elseif h > 12 then
+                mt = pt_hs[h - 12] .. " e meia"
+            else
+                mt = pt_hs[h] .. " e meia"
+            end
+        else
+            if h == 0 then
+                mt = "meia-noite"
+            elseif h == 12 then
+                mt = "meio-dia"
+            elseif h > 12 then
+                mt = pt_hs[h - 12]
+            else
+                mt = pt_hs[h]
+            end
+            if m ~= 0 then
+                mt = mt .. " e " .. m
+            end
+            if h == 0 or h == 12 then
+                return mt
+            end
+        end
+        if h < 12 then
+            return mt .. " da manhã"
+        elseif h > 13 and h < 19 then
+            return mt .. " da tarde"
+        else
+            return mt .. " da noite"
+        end
+    end
+end
+
+
+-- Formal portuguese.
+
+function pt_formal(h, m)
+    local hs = ""
+    if h == 0 then
+        hs = "zero hora"
+    elseif h == 1 then
+        hs = "uma hora"
+    else
+        hs = h .. " horas"
+    end
+    if m == 1 then
+        hs = hs .. " e um minuto"
+    elseif m > 1 then
+        hs = hs .. " e " .. m .. "minutos"
+    end
+    return hs
+end
